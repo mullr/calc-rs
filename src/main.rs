1b90428feacf9de570a783fbb31b663e0370ed29
Russell Mull <russell@digitalartificer.com>
2018-04-23 19:48:55 -0700

    Support add

--- src/main.rs
+++ src/main.rs
@@ -39,8 +39,6 @@ fn to_op(p: pest::iterators::Pair<Rule>) -> Result<Op, CalcParseError> {
     }
 }
 
-
-
 fn to_expr(p: pest::iterators::Pair<Rule>) -> Result<Expr, CalcParseError> {
     match p.as_rule() {
 
@@ -49,19 +47,20 @@ fn to_expr(p: pest::iterators::Pair<Rule>) -> Result<Expr, CalcParseError> {
             .map(|x| Expr::Int(x))
             .map_err(|e| CalcParseError::IntParse),
 
-        Rule::mult_expr => {
+        Rule::mult_expr | Rule::add_expr => {
             let mut pairs = p.into_inner();
             let lhs = to_expr(pairs.next().ok_or(CalcParseError::NotEnoughInput)?)?;
             let op = to_op(pairs.next().ok_or(CalcParseError::NotEnoughInput)?)?;
             let rhs = to_expr(pairs.next().ok_or(CalcParseError::NotEnoughInput)?)?;
             Ok(Expr::Binary(op, Box::new(lhs), Box::new(rhs)))
-        }
+        },
+
         _ => Err(CalcParseError::UnknownRule),
     }
 }
 
 fn main() {
-    let pairs = CalcParser::parse(Rule::mult_expr_or_int, "12*13").unwrap();
+    let pairs = CalcParser::parse(Rule::expr, "1*2+3*4").unwrap();
     for p in pairs {
         let e = to_expr(p);
         println!("{:?}", e);
